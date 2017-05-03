d378fb988796903283bee11af26de591e405e9ba
AwfulInThis3itch <awfulinthisbiitch@gmail.com>
2017-05-03 18:29:08 +1200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by User on 5/1/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Time.java
@@ -0,0 +1,94 @@
+/**
+ * Created by User on 5/1/2017.
+ */
+public class Time implements Comparable<Time> {
+
+    private int hours;
+    private int mins;
+    private int secs;
+
+    public Time(int hours, int mins, int secs) {
+        if(hours>= 23 || hours >=0 ){
+            this.hours = hours;
+        }else{
+            this.hours = 0;
+            System.out.println("Wrong amount in Hours, Setting to 0");
+        }
+        if(mins >= 0 || mins <=59 ){
+            this.mins = mins;
+        }else {
+            this.mins = 0;
+            System.out.println("Wrong amount in Mins, Setting to 0");
+        }
+        if (secs >= 0 || mins <= 59){
+            this.secs = secs;
+        }else{
+            this.secs = 0;
+            System.out.println("Wrong amount in Secs, Setting to 0");
+        }
+
+    }
+
+    public Time(int hours) {
+        if(hours>= 23 || hours >=0 ){
+            this.hours = hours;
+        }else{
+            this.hours = 0;
+            System.out.println("Wrong amount in hours, Setting to 0");
+        }
+    }
+
+    public Time(int hours, int mins) {
+        if(hours>= 23 || hours >=0 ){
+            this.hours = hours;
+        }else{
+            this.hours = 0;
+            System.out.println("Wrong amount in hours, Setting to 0");
+        }
+        if(mins >= 0 || mins <=59 ){
+            this.mins = mins;
+        }else{
+            this.mins = 0;
+            System.out.println("Wrong amount in mins, Setting to 0");
+        }
+    }
+
+    public Time(){
+        this.hours = 0;
+        this.mins = 0;
+        this.secs = 0;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMins() {
+        return mins;
+    }
+
+    public void setMins(int mins) {
+        this.mins = mins;
+    }
+
+    public int getSecs() {
+        return secs;
+    }
+
+    public void setSecs(int secs) {
+        this.secs = secs;
+    }
+
+   public String toString (){
+        return this.hours + " : "+ this.mins + " : " + this.secs;
+   }
+
+    @Override
+    public boolean equals(Time time) {
+      if(time.compareTo(time.hours))
+    }
+}
